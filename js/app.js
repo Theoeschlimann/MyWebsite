3897c20daf196a9a760f1fc1e4f7e3da33b99a15
Theo Eschlimann <theo.eschlimann@gmail.com>
2020-06-21 15:34:17 +0200

    darkMode desktop

---
+++ js/app.js
@@ -0,0 +1,10 @@
+const body = document.querySelector('body');
+const darkModeBg = document.querySelector('.darkMode-bg');
+const darkModeIcon = document.querySelector('.darkMode-icon');
+const contactButton = document.querySelector('.contact-button');
+
+darkModeIcon.onclick = function(){
+    darkModeBg.classList.toggle("darkMode-bg-active");
+    contactButton.classList.toggle("contact-button-darkMode");
+    body.classList.toggle("light");
+};
