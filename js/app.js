bcb09de6189c60e0c5dd9b52bca497ee200bccde
Theo <theo.eschlimann@gmail.com>
2020-06-27 15:56:05 +0200

    Cantact hover wizz

--- js/app.js
+++ js/app.js
@@ -1,10 +1,19 @@
+
+//DARKMODE
 const body = document.querySelector('body');
 const darkModeBg = document.querySelector('.darkMode-bg');
 const darkModeIcon = document.querySelector('.darkMode-icon');
 const contactButton = document.querySelector('.contact-button');
 
-darkModeIcon.onclick = function(){
+darkModeIcon.onclick = function () {
     darkModeBg.classList.toggle("darkMode-bg-active");
     contactButton.classList.toggle("contact-button-darkMode");
     body.classList.toggle("light");
 };
+
+//WIZZSOUND
+function PlaySound(soundobj) {
+    var thissound = document.getElementById(soundobj);
+    thissound.play();
+};
+
