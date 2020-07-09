9a528a5626db4b29e53389967a89b0eff0a2e837
Theo Eschlimann <theo.eschlimann@gmail.com>
2020-07-09 13:54:08 +0200

    Update app.js

--- js/app.js
+++ js/app.js
@@ -7,4 +7,12 @@ darkModeIcon.onclick = function(){
     darkModeBg.classList.toggle("darkMode-bg-active");
     contactButton.classList.toggle("contact-button-darkMode");
     body.classList.toggle("light");
+
+    // if (body.style.color = "black"){
+    //     body.style.color = "white";
+    //     console.log("is black");
+    // } else if(body.style.color = "white"){
+    //     body.style.color ="black";
+    //     console.log("is white");
+    // }
 };
